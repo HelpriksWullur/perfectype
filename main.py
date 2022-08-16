7b3923829cad632ff15beefa9ef507e7dd56db11
Helpriks Wullur <wullurhelpriks@gmail.com>
2022-08-16 20:11:42 +0700

    menambahkan fungsi pengecekan sederhana

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import tkinter
 from tkinter import *
+
 
 WIDTH = 640
 HEIGHT = 480
+
+test_text = "Hello World"
+
+
+def check(ev):
+    if ev.keycode != 22:
+        if len(txt.get("1.0", "end-1c")) == len(in_txt.get("1.0", "end-1c") + ev.char):
+            if txt.get("1.0", "end-1c") == in_txt.get("1.0", "end-1c") + ev.char:
+                print("100% akurat")
+            else:
+                print("belum akurat")
+
 
 root = Tk()
 root.title("Perfectype")
@@ -12,11 +26,13 @@ root.resizable(False, False)
 
 Label(text="Text").pack()
 txt = Text(root, width=100, height=10)
+txt.insert(tkinter.END, test_text)
 
 txt.pack()
 
 Label(text="Input").pack()
 in_txt = Text(root, width=100, height=10)
+in_txt.bind('<Key>', check)
 
 in_txt.pack()
 

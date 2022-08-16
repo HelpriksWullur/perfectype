1992692f441322997e8d1a0391d5a450a5368e35
Helpriks Wullur <wullurhelpriks@gmail.com>
2022-08-16 20:01:35 +0700

    menambahkan 2 component label dan 2 component text

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
-WIDTH = 600
-HEIGHT = 400
+WIDTH = 640
+HEIGHT = 480
 
 root = Tk()
 root.title("Perfectype")
@@ -9,5 +9,15 @@ root.geometry("{}x{}+{}+{}".format(WIDTH, HEIGHT,
                                    root.winfo_screenwidth() // 2 - WIDTH // 2,
                                    root.winfo_screenheight() // 2 - HEIGHT // 2))
 root.resizable(False, False)
+
+Label(text="Text").pack()
+txt = Text(root, width=100, height=10)
+
+txt.pack()
+
+Label(text="Input").pack()
+in_txt = Text(root, width=100, height=10)
+
+in_txt.pack()
 
 root.mainloop()
